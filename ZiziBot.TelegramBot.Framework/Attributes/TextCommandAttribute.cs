f4f9fc3fac99160830a7b5f3e174e91c85eb1f86
Azhe Kun <root@corp.azhe.my.id>
2024-10-18 22:06:38 +0700

    text command

--- ZiziBot.TelegramBot.Framework/Attributes/TextCommandAttribute.cs
+++ ZiziBot.TelegramBot.Framework/Attributes/TextCommandAttribute.cs
@@ -1,10 +1,12 @@
 ﻿using JetBrains.Annotations;
+using ZiziBot.TelegramBot.Framework.Models.Enums;
 
 namespace ZiziBot.TelegramBot.Framework.Attributes;
 
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
 [MeansImplicitUse]
-public class TextCommandAttribute(string command) : Attribute
+public class TextCommandAttribute(string command, ComparisonType comparisonType = ComparisonType.Match) : Attribute
 {
     public string Command { get; } = command;
+    public ComparisonType ComparisonType { get; } = comparisonType;
 }
---
+++ ZiziBot.TelegramBot.Framework/Models/Enums/ComparisonType.cs
@@ -0,0 +1,10 @@
+﻿namespace ZiziBot.TelegramBot.Framework.Models.Enums;
+
+public enum ComparisonType
+{
+    Match,
+    CommandLike,
+    Pattern,
+    Contains,
+    Regex
+}
